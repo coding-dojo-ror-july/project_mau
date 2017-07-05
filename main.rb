b197b851cec8a7769a66ce2f63843cd57670e21e
Mauricio Ruanova-Hurtado <mruanova@users.noreply.github.com>
2017-07-05 13:45:47 -0500

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,20 @@
+# Assignment: Project
+# Create a Project Class that has the following attributes: name, description.
+# Also, create a instance method called elevator_pitch
+# that will print out the name of the project and its description separated by a comma.
+# Example:
+class Project
+    def initialize(name, description)
+        @name = name
+        @description = description
+    end
+    def name
+        @name
+    end
+    def elevator_pitch
+        puts "#{@name},#{@description}"
+    end
+end
+project1 = Project.new("Project 1", "Description 1")
+puts project1.name # => "Project 1"
+project1.elevator_pitch  # => "Project 1, Description 1"
